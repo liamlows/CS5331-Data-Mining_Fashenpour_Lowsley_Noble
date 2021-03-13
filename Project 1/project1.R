3901f9d518e068a4761334e95e2f23d5576462a0
liamlows <liam.lowsleywilliams@gmail.com>
2021-03-13 16:04:27 -0600

    v1.2

--- Project 1/project1.R
+++ Project 1/project1.R
@@ -16,10 +16,44 @@ cases_TX$date <- ymd(cases_TX$date)
 lapply(cases_TX, class)
 # check for NA values
 lapply(cases_TX, anyNA)
+# remove instances up to first recorded case in any county
+cases_TX <- subset(cases_TX, date >= "2020-03-05")
 
 # AT THIS POINT NO NA VALUES AND TYPES ARE CORRECT
 
 summary(cases_TX)
+head(cases_TX, 20)
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+mean(cases_TX$confirmed_cases)
+median(cases_TX$confirmed_cases)
+Mode(cases_TX$confirmed_cases)
+sd(cases_TX$confirmed_cases)
+var(cases_TX$confirmed_cases)
+
+mean(cases_TX$deaths)
+median(cases_TX$deaths)
+Mode(cases_TX$deaths)
+sd(cases_TX$deaths)
+var(cases_TX$deaths)
+
+Mode(cases_TX$county_fips_code)
+table(cases_TX$county_fips_code)
+Mode(cases_TX$county_name)
+table(cases_TX$county_name)
+
+Mode(cases_TX$state)
+table(cases_TX$state)
+Mode(cases_TX$state_fips_code)
+table(cases_TX$state_fips_code)
+
+
+table(cases_TX$confirmed_cases)[1]
+table(cases_TX$deaths)[1]
+
 
 # check for duplicate/missing counties in TX - there are none
 length(unique(cases_TX$county_fips_code))
@@ -74,9 +108,55 @@ length(unique(cases_TX_census$county_name))
 # check for non texas entries - there are none
 length(unique(cases_TX_census$state))
 
+multi_stats <- function(x){
+  c(mean = mean(x), median = median(x), mode = Mode(x), freq = table(x)[x[x==Mode(x)]], sd = sd(x), var = var(x), min = min(x), max = max(x) )
+}
+
+
+
+test1 <- lapply(cases_TX_census, FUN = multi_stats)
+
+lapply(cases_TX_census, FUN = mean)
+lapply(cases_TX_census, FUN = median)
+lapply(cases_TX_census, FUN = Mode)
+lapply(cases_TX_census, FUN = sd)
+lapply(cases_TX_census, FUN = var)
+
+
+
+mean(cases_TX$confirmed_cases)
+median(cases_TX$confirmed_cases)
+Mode(cases_TX$confirmed_cases)
+table(cases_TX$county_fips_code)
+sd(cases_TX$confirmed_cases)
+var(cases_TX$confirmed_cases)
+
+
+mean(cases_TX$deaths)
+median(cases_TX$deaths)
+Mode(cases_TX$deaths)
+sd(cases_TX$deaths)
+var(cases_TX$deaths)
+
+Mode(cases_TX$county_fips_code)
+table(cases_TX$county_fips_code)
+Mode(cases_TX$county_name)
+table(cases_TX$county_name)
+
+Mode(cases_TX$state)
+table(cases_TX$state)
+Mode(cases_TX$state_fips_code)
+table(cases_TX$state_fips_code)
+
+
+table(cases_TX$confirmed_cases)[1]
+table(cases_TX$deaths)[1]
+
 subset(cases_TX_census, black_pop == 0)
 
+subset(cases_TX_census, hispanic_pop == 12)
 
+subset(cases_TX_census, white_pop == 55)
 
 
 head(cases_US_df)
