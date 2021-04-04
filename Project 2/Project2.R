f584336b7c6db4e1e00aa5c3d25f16f31b1ef6f2
liamlows <liam.lowsleywilliams@gmail.com>
2021-04-03 23:19:23 -0500

    more clustering 2

--- Project 2/Project2.R
+++ Project 2/Project2.R
@@ -218,29 +218,104 @@ ggplot(public_commuters_cases_deaths_hccluster, aes(long, lat)) +
 
 ################################# Cluster 2 #################################
 
-pop_fatality <- dataset %>%
+cases_deaths_fatality <- dataset %>%
   select(
-    fatality_rate,
-    total_pop
+    cases_per_1000,
+    deaths_per_1000,
+    fatality_rate
   ) %>% scale() %>% as_tibble()
 
-summary(pop_fatality)
+# FIND BEST CLUSTER SIZE
+set.seed(1234)
+ks <- 2:10
 
-km2 <- kmeans(pop_fatality, centers = 4)
+d2 <- dist(cases_deaths_fatality)
+
+# finding best cluster number by knee
+WSS <- sapply(ks, FUN = function(k) {
+  kmeans(cases_deaths_fatality, centers = k, nstart = 5)$tot.withinss
+})
+ggplot(as_tibble(ks, WSS), aes(ks, WSS)) + geom_line() +
+  labs(title = "Kmeans Within Sum Squared of Clusters For Cases/Deaths Per 1000 And Fatality Rate")
+
+# finding best cluster number by silhouette width
+ASW <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d2, kmeans(cases_deaths_fatality, centers=k, nstart = 5)$cluster)$avg.silwidth
+})
+best_k_ASW <- ks[which.max(ASW)]
+ggplot(as_tibble(ks, ASW), aes(ks, ASW)) + geom_line() +
+  geom_vline(xintercept = best_k_ASW, color = "red", linetype = 2) +
+  labs(title = "Kmeans Average Silhoutte Width of Clusters For Cases/Deaths Per 1000 And Fatality Rate")
+
+# finding best cluster number by Dunn index
+DI <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d2, kmeans(cases_deaths_fatality, centers=k, nstart=5)$cluster)$dunn
+})
+best_k_DI <- ks[which.max(DI)]
+ggplot(as_tibble(ks, DI), aes(ks, DI)) + geom_line() +
+  geom_vline(xintercept = best_k_DI, color = "red", linetype = 2) +
+  labs(title = "Kmeans Dunn Index of Clusters For Cases/Deaths Per 1000 And Fatality Rate")
+
+# k-means clustering
+summary(cases_deaths_fatality)
+
+km2 <- kmeans(cases_deaths_fatality, centers = 6)
 km2
 
-ggplot(pivot_longer(as_tibble(km2$centers,  rownames = "cluster"), cols = colnames(km2$centers)), aes(y = name, x = value)) + 
+ggplot(pivot_longer(as_tibble(km2$centers,  rownames = "cluster"), cols = colnames(km2$centers)), aes(y = name, x = value)) +
   geom_bar(stat = "identity") +
-  facet_grid(rows = vars(cluster))
+  facet_grid(rows = vars(cluster)) +
+  labs(title = "Kmeans Cluster Centers Summary For Cases/Deaths Per 1000 And Fatality Rate")
 
-pop_fatality_cluster <- counties_TX %>%
+cases_deaths_fatality_cluster <- counties_TX %>%
   left_join(dataset %>% add_column(cluster = factor(km2$cluster)))
 
-ggplot(pop_fatality_cluster, aes(long, lat)) + 
+ggplot(cases_deaths_fatality_cluster, aes(long, lat)) + 
   geom_polygon(aes(group = group, fill = cluster)) +
   coord_quickmap() + 
   scale_fill_viridis_d() + 
-  labs(title = "Clusters of total population and fatality rate")
+  labs(title = "Kmeans Clusters of Cases/Deaths Per 1000 And Fatality Rate")
+
+cases_deaths_fatality_stats <- dataset %>% add_column(cluster = factor(km2$cluster))
+
+cases_deaths_fatality_stats %>% group_by(cluster) %>% summarize(
+  avg_poverty = mean(poverty_1000), 
+  avg_income = mean(median_income),
+  avg_income_capita = mean(income_per_capita))
+
+# hierarchical clustering
+hc2 <- hclust(d2, method = "complete")
+ggdendrogram(hc2, labels = FALSE, theme_dendro = FALSE)
+hc2_clusters <- cutree(hc2, k = 6)
+hc2_centers <- sapply(unique(hc2_clusters), clust.centroid, cases_deaths_fatality, hc2_clusters)
+
+hc2_centers <- t(hc2_centers)
+rownames(hc2_centers) <- seq(from=1, to=6, by=1)
+
+ggplot(pivot_longer(as_tibble(hc2_centers,  rownames = "cluster"), cols = colnames(hc2_centers)), aes(y = name, x = value)) +
+  geom_bar(stat = "identity") +
+  facet_grid(rows = vars(cluster)) +
+  labs(title = "Hierarchical Cluster Centers Summary For Cases/Deaths Per 1000 And Fatality Rate")
+
+
+cases_deaths_fatality_hccluster <- counties_TX %>%
+  left_join(dataset %>% add_column(cluster = factor(hc2_clusters)))
+
+rownames(cases_deaths_fatality) <- dataset$county_name
+
+factoextra::fviz_cluster(list(data = cases_deaths_fatality, cluster = hc2_clusters)) +
+  labs(title = "Hierarchical Clusters of Cases/Deaths Per 1000 And Fatality Rate")
+
+ggplot(cases_deaths_fatality_hccluster, aes(long, lat)) + 
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() + 
+  scale_fill_viridis_d() + 
+  labs(title = "Hierarchical Clusters of Cases/Deaths Per 1000 And Fatality Rate")
+
+cases_deaths_fatality_hccluster %>% group_by(cluster) %>% summarize(
+  avg_poverty = mean(poverty_1000), 
+  avg_income = mean(median_income),
+  avg_income_capita = mean(income_per_capita))
 
 ################################# cluster 3 #################################
 
@@ -278,7 +353,6 @@ ggplot(scaled_TX_race_cluster, aes(long, lat)) +
 
 scaled_TX_race_1000 <- dataset %>% 
   select(
-    median_income,
     white_per_1000,
     black_per_1000,
     asian_per_1000,
@@ -339,4 +413,208 @@ ggplot(edu_income_index_virus_cluster, aes(long, lat)) +
   labs(title = "Clusters of education, income/poverty, GINI index, and case/death counts")
 
 ################################# cluster 6 #################################
+
+race_county <- dataset %>%
+  select(
+    white_per_1000,
+    black_per_1000,
+    asian_per_1000,
+    hispanic_per_1000,
+    amerindian_per_1000,
+    other_per_1000
+  ) %>% scale() %>% as_tibble()
+
+# FIND BEST CLUSTER SIZE
+set.seed(1234)
+ks <- 2:10
+
+d6 <- dist(race_county)
+str(d6)
+
+# finding best cluster number by knee
+WSS <- sapply(ks, FUN = function(k) {
+  kmeans(race_county, centers = k, nstart = 5)$tot.withinss
+})
+ggplot(as_tibble(ks, WSS), aes(ks, WSS)) + geom_line() +
+  labs(title = "Kmeans Within Sum Squared of Clusters For Commuters By Public Transportation and Case/Death Counts")
+
+# finding best cluster number by silhouette width
+ASW <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d6, kmeans(race_county, centers=k, nstart = 5)$cluster)$avg.silwidth
+})
+best_k_ASW <- ks[which.max(ASW)]
+ggplot(as_tibble(ks, ASW), aes(ks, ASW)) + geom_line() +
+  geom_vline(xintercept = best_k_ASW, color = "red", linetype = 2) +
+  labs(title = "Kmeans Average Silhoutte Width of Clusters For Commuters By Public Transportation and Case/Death Counts")
+
+# finding best cluster number by Dunn index
+DI <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d6, kmeans(race_county, centers=k, nstart=5)$cluster)$dunn
+})
+best_k_DI <- ks[which.max(DI)]
+ggplot(as_tibble(ks, DI), aes(ks, DI)) + geom_line() +
+  geom_vline(xintercept = best_k_DI, color = "red", linetype = 2) +
+  labs(title = "Kmeans Dunn Index of Clusters For Commuters By Public Transportation and Case/Death Counts")
+
+# k-means clustering
+summary(fatality_deaths_cases)
+
+km6 <- kmeans(race_county, centers = 6)
+km6
+
+ggplot(pivot_longer(as_tibble(km6$centers,  rownames = "cluster"), cols = colnames(km6$centers)), aes(y = name, x = value)) +
+  geom_bar(stat = "identity") +
+  facet_grid(rows = vars(cluster)) +
+  labs(title = "Kmeans Cluster Centers Summary For Commuters By Public Transportation and Case/Death Counts")
+
+race_county_cluster <- counties_TX %>%
+  left_join(dataset %>% add_column(cluster = factor(km6$cluster)))
+
+ggplot(race_county_cluster, aes(long, lat)) + 
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() + 
+  scale_fill_viridis_d() + 
+  labs(title = "Kmeans Clusters of Commuters By Public Transportation and Case/Death Counts")
+
+cases_TX_km <- dataset %>% add_column(cluster = factor(km6$cluster))
+
+cases_TX_km %>% group_by(cluster) %>% summarize(
+  avg_cases = mean(cases_per_1000), 
+  avg_deaths = mean(deaths_per_1000),
+  avg_fatality = mean(fatality_rate))
+
+# hierarchical clustering
+hc1 <- hclust(d6, method = "complete")
+ggdendrogram(hc1, labels = FALSE, theme_dendro = FALSE)
+clusters <- cutree(hc1, k = 4)
+
+clust.centroid = function(i, dat, clusters) {
+  ind = (clusters == i)
+  colMeans(dat[ind,])
+}
+centers <- sapply(unique(clusters), clust.centroid, public_commuters_cases_deaths, clusters)
+
+centers_inv <- t(centers)
+rownames(centers_inv) <- c(1,2,3,4)
+
+ggplot(pivot_longer(as_tibble(centers_inv,  rownames = "cluster"), cols = colnames(centers_inv)), aes(y = name, x = value)) +
+  geom_bar(stat = "identity") +
+  facet_grid(rows = vars(cluster)) +
+  labs(title = "Hierarchical Cluster Centers Summary For Commuters By Public Transportation and Case/Death Counts")
+
+
+public_commuters_cases_deaths_hccluster <- counties_TX %>%
+  left_join(dataset %>% add_column(cluster = factor(clusters)))
+
+rownames(public_commuters_cases_deaths) <- dataset$county_name
+
+factoextra::fviz_cluster(list(data = public_commuters_cases_deaths, cluster = clusters)) +
+  labs(title = "Hierarchical Clusters of Commuters By Public Transportation and Case/Death Counts")
+
+ggplot(public_commuters_cases_deaths_hccluster, aes(long, lat)) + 
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() + 
+  scale_fill_viridis_d() + 
+  labs(title = "Hierarchical Clusters of Commuters By Public Transportation and Case/Death Counts")
+
+################################# cluster 7 #################################
+
+poverty_commuters_age <- dataset %>%
+  select(
+    poverty_1000,
+    commuters_public_1000,
+    median_age
+  ) %>% scale() %>% as_tibble()
+
+# FIND BEST CLUSTER SIZE
+set.seed(1234)
+ks <- 2:10
+
+d7 <- dist(poverty_commuters_age)
+
+# finding best cluster number by knee
+WSS <- sapply(ks, FUN = function(k) {
+  kmeans(poverty_commuters_age, centers = k, nstart = 5)$tot.withinss
+})
+ggplot(as_tibble(ks, WSS), aes(ks, WSS)) + geom_line() +
+  labs(title = "Kmeans Within Sum Squared of Clusters For Commuters By Public Transportation and Case/Death Counts")
+
+# finding best cluster number by silhouette width
+ASW <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d7, kmeans(poverty_commuters_age, centers=k, nstart = 5)$cluster)$avg.silwidth
+})
+best_k_ASW <- ks[which.max(ASW)]
+ggplot(as_tibble(ks, ASW), aes(ks, ASW)) + geom_line() +
+  geom_vline(xintercept = best_k_ASW, color = "red", linetype = 2) +
+  labs(title = "Kmeans Average Silhoutte Width of Clusters For Commuters By Public Transportation and Case/Death Counts")
+
+# finding best cluster number by Dunn index
+DI <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d7, kmeans(poverty_commuters_age, centers=k, nstart=5)$cluster)$dunn
+})
+best_k_DI <- ks[which.max(DI)]
+ggplot(as_tibble(ks, DI), aes(ks, DI)) + geom_line() +
+  geom_vline(xintercept = best_k_DI, color = "red", linetype = 2) +
+  labs(title = "Kmeans Dunn Index of Clusters For Commuters By Public Transportation and Case/Death Counts")
+
+# k-means clustering
+summary(poverty_commuters_age)
+
+km7 <- kmeans(poverty_commuters_age, centers = 6)
+km7
+
+ggplot(pivot_longer(as_tibble(km7$centers,  rownames = "cluster"), cols = colnames(km7$centers)), aes(y = name, x = value)) +
+  geom_bar(stat = "identity") +
+  facet_grid(rows = vars(cluster)) +
+  labs(title = "Kmeans Cluster Centers Summary For Commuters By Public Transportation and Case/Death Counts")
+
+poverty_commuters_age_cluster <- counties_TX %>%
+  left_join(dataset %>% add_column(cluster = factor(km7$cluster)))
+
+ggplot(poverty_commuters_age_cluster, aes(long, lat)) + 
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() + 
+  scale_fill_viridis_d() + 
+  labs(title = "Kmeans Clusters of Commuters By Public Transportation and Case/Death Counts")
+
+poverty_commuters_age_stats <- dataset %>% add_column(cluster = factor(km7$cluster))
+
+poverty_commuters_age_stats %>% group_by(cluster) %>% summarize(
+  avg_cases = mean(cases_per_1000), 
+  avg_deaths = mean(deaths_per_1000),
+  avg_fatality = mean(fatality_rate))
+
+# hierarchical clustering
+hc7 <- hclust(d7, method = "complete")
+ggdendrogram(hc7, labels = FALSE, theme_dendro = FALSE)
+hc7_clusters <- cutree(hc7, k = 6)
+hc7_centers <- sapply(unique(hc7_clusters), clust.centroid, poverty_commuters_age, hc7_clusters)
+
+hc7_centers <- t(hc7_centers)
+rownames(hc7_centers) <- seq(from=1, to=6, by=1)
+
+ggplot(pivot_longer(as_tibble(hc7_centers,  rownames = "cluster"), cols = colnames(hc7_centers)), aes(y = name, x = value)) +
+  geom_bar(stat = "identity") +
+  facet_grid(rows = vars(cluster)) +
+  labs(title = "Hierarchical Cluster Centers Summary For Commuters By Public Transportation and Case/Death Counts")
+
+
+poverty_commuters_age_hccluster <- counties_TX %>%
+  left_join(dataset %>% add_column(cluster = factor(hc7_clusters)))
+
+rownames(public_commuters_cases_deaths) <- dataset$county_name
+
+factoextra::fviz_cluster(list(data = poverty_commuters_age, cluster = hc7_clusters)) +
+  labs(title = "Hierarchical Clusters of Commuters By Public Transportation and Case/Death Counts")
+
+ggplot(poverty_commuters_age_hccluster, aes(long, lat)) + 
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() + 
+  scale_fill_viridis_d() + 
+  labs(title = "Hierarchical Clusters of Commuters By Public Transportation and Case/Death Counts")
+
+poverty_commuters_age_hccluster %>% group_by(cluster) %>% summarize(
+  avg_cases = mean(cases_per_1000), 
+  avg_deaths = mean(deaths_per_1000),
+  avg_fatality = mean(fatality_rate))
 
