cc6c6f87eca035dd583276e008dc866741545430
liamlows <liam.lowsleywilliams@gmail.com>
2021-05-01 17:54:08 -0500

    v1.2

--- Project 3/Project3.R
+++ Project 3/Project3.R
@@ -6,6 +6,7 @@ library(seriation)
 library(cluster)
 library(factoextra)
 library(caret)
+library(FSelector)
 # -----------------------------------------------------------------------------------
 # STAT FUNCTIONS
 Mode <- function(x) {
@@ -22,7 +23,7 @@ Rnge <- function(x) {
 }
 # -----------------------------------------------------------------------------------
 # read in US data w/ census csv
-cases_US_census <- read.csv("./Projects/Project\ 3/data/COVID-19_cases_plus_census.csv")
+cases_US_census <- read.csv("./Projects/Project\ 3/data/04-24-21_COVID-CENSUS.csv")
 # subset to include chosen data attributes
 cases_US_census <- subset(cases_US_census, select = c("county_name",
                                                       "state",
@@ -78,18 +79,19 @@ dataset <- cases_US_census %>% mutate(
   high_school_including_ged = high_school_including_ged/total_pop*1000,
   fatality_rate = deaths/confirmed_cases
 )
-
-# remove NA values because 0/0
-dataset[which(is.na(dataset$fatality_rate)),]$fatality_rate <- 0
-
-colnames(dataset)
-head(dataset)
 summary(dataset)
+# remove NA values because 0/0
+dataset[which(is.na(dataset$fatality_rate)),]
+dataset[which(is.na(dataset$fatality_rate)),]$fatality_rate <- 0
+# check
+summary(dataset)
+
 
 # counties <- as_tibble(map_data("county"))
 # counties_TX <- counties %>% dplyr::filter(region == "texas") %>% rename(c(county = subregion))
 # dataset <- dataset %>% mutate(county = county_name %>% str_to_lower() %>% str_replace('\\s+county\\s*$', ''))
 
+# statistic functions
 lapply(dataset, FUN = mean)
 lapply(dataset, FUN = median)
 lapply(dataset, FUN = Mode)
@@ -100,20 +102,62 @@ lapply(dataset, FUN = min)
 lapply(dataset, FUN = max)
 lapply(dataset, FUN = Rnge)
 
-dataset
+# deaths per 1000 > 1.8
+dataset_sel <- dataset %>% mutate(fatal = as.factor(deaths > 1.8))
 
-subset(dataset, select=c(confirmed_cases == 0))
+# county names in Louisiana contain 'Parish' at the end, need to remove them to
+# match the map counties
+la_county <- dataset_sel$county_name[dataset_sel$state == 'LA']
+la_county <- gsub("[.]","",la_county) 
+la_county
+la_county <- gsub("\\s*\\w*$", "", la_county)
+la_county
 
-
-dataset[which(is.na(dataset$fatality_rate)),]
-
-
-dataset_sel <- dataset %>% mutate(fatal = as.factor(deaths_per_1000 > 1))
+dataset_sel$county_name[dataset_sel$state == 'LA'] <- la_county
+dataset_sel$county_name[dataset_sel$state == 'LA']
 
 head(dataset_sel)
 
+# balance is ok
 dataset_sel %>% pull(fatal) %>% table()
 
+# states affected worst
+dataset_sel %>% group_by(state) %>% 
+  summarize(fatal_pct = sum(fatal == TRUE)/n()) %>%
+  arrange(desc(fatal_pct))
 
+# train is not TX
+cases_train1 <- dataset_sel %>% filter(!(state %in% c("TX")))
+cases_train1 %>% pull(fatal) %>% table()
+
+# test is TX
+cases_test1 <-  dataset_sel %>% filter((state %in% c("TX")))
+cases_test1 %>% pull(fatal) %>% table()
+
+# obtain counties
+counties <- as_tibble(map_data("county"))
+counties <- counties %>% 
+  rename(c(county = subregion, state = region)) %>%
+  mutate(state = state.abb[match(state, tolower(state.name))]) %>%
+  select(state, county, long, lat, group)
+counties
+
+
+
+# add county names to map
+counties_all <- counties %>% left_join(cases_train1 %>% 
+    mutate(county = county_name %>% str_to_lower() %>% 
+    str_replace('\\s+county\\s*$', '')))
+
+# plot
+ggplot(counties_all, aes(long, lat)) + 
+  geom_polygon(aes(group = group, fill = fatal), color = "black", size = 0.1) + 
+  coord_quickmap() + scale_fill_manual(values = c('TRUE' = 'red', 'FALSE' = 'grey')) +
+  labs(title = "U.S. Map of Deaths Per 1000", subtitle = "Red = greater than 1.8 per 1000, Grey = less than 1.8 per 1000")
+
+colnames(cases_train1)
+
+# check variable importance
+cases_train1 <- cases_train1 %>% select(-deaths, -confirmed_cases, fatality_rate)
 
 
